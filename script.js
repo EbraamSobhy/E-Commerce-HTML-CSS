512d7bf151ed5810810f2a086138870a2969a0fd
EbraamSobhy <142206373+EbraamSobhy@users.noreply.github.com>
2024-03-19 16:45:45 +0200

    my changes

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("This is the change Test");
+// Prompting the user for their name
+var userName = prompt("What's your name?");
+
+// Greeting the user
+alert("Hello, " + userName + "! Welcome to our website.");
